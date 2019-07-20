3003b6992271b5b2503f3209c2fd6435d8a9793b
shengchao9505 <shengchao9505@163.com>
2019-07-20 09:58:44 +0800

    install packages from bioconductor

---
+++ bioconducter.R
@@ -0,0 +1,8 @@
+#安装来源于bioconductor的R包
+source('http://bioconductor.org/biocLite.R')
+biocLite('survcomp')
+library(survcomp)
+##rafalib包
+install.packages('rafalib')
+library(rafalib)
+install_bioc('survcomp')
