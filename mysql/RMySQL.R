7c92674e5b03aad025f29e3988a60ac46c617a09
shengchao12345 <shengchao9505@163.com>
2019-10-15 20:12:43 +0800

    analysis of tumor marker by Mysql

--- mysql/RMySQL.R
+++ mysql/RMySQL.R
@@ -11,7 +11,11 @@ cnn<-dbConnect(MySQL(),host='49.232.130.131',user='root',password='',dbname='scr
 encoding <- if(grepl(pattern = 'utf8|utf-8',x = Sys.getlocale(),ignore.case = T)) 'utf8' else 'latin1'
 dbSendQuery(cnn,paste("SET names",encoding))
 #读取数据集biomark2019
-biomark<-dbReadTable(cnn,'biomarker')
+biomark<-dbReadTable(cnn,'biomarker')#17+18+19的biomarker数据库
+biomark<-dbGetQuery(cnn,'SELECT id,name,AFP,CA199,CA153,CA125,CEA,PG1,PG2,PGR,HBsAg 
+                    FROM biomarker WHERE year=2019;')#19年tumor marker数据
+biomark<-dbGetQuery(cnn,'SELECT id,name,sex,age,AFP,CA199,CA153,CA125,CEA,PG1,PG2,PGR,HBsAg 
+                    FROM biomarker WHERE year=2019 and source="示范区";')#19年示范区tumor marker数据
 baseline<-dbReadTable(cnn,'baseline2019')
 baseline<-dbReadTable(cnn,'baseline')#读取17+18+19基线数据
 #读取baseline1718
@@ -31,7 +35,7 @@ str_func<-function(x){
   b=table(str_detect(x,'>'))
   return(b[2])
   }
-biomark1.1<-biomark%>%select(AFP,CA199,CA153,CA125,CEA,PG1,PG2,PG1.PG2,NSE,SCC,Fer)
+biomark1.1<-biomark%>%select(AFP,CA199,CA153,CA125,CEA,PG1,PG2,PGR,NSE,SCC,Fer)
 apply(biomark1.1,2,str_func)#查看各个指标中的极大值数量
 #重新赋值极大值
 #1.将PG1＞200去除
@@ -84,43 +88,87 @@ apply(biomark1.1[,c('afp.pos','cea.pos','ca199.pos','ca125.pos','ca153.pos','PG.
 with(biomark1.1,table(area,postive))
 #----------------------------------------5.2019胃蛋白酶原基础性分析----------------------------------------------------------------------------
 #1.首先分析和排除极大值
-biomark5.1<-biomark%>%filter(year==2019 & source=='示范区')%>%select(sex,age,PG1,PG2,PGR)#脱敏
+biomark5.1<-biomark%>%select(PG1,PG2,PGR)#脱敏6030
 apply(biomark5.1,2,str_func)#查看各个指标中的极大值数量
-biomark5.3<-biomark5.1%>%filter(PG1!='>200.0')#去除PG1>200的，89个，剩5920个
+biomark5.3<-biomark5.1%>%filter(PG1!='>200.0')#去除PG1>200的，89个，剩5941个
 biomark5.3[,c('PG1','PG2','PGR')]<-data.frame(apply(biomark5.3[,c('PG1','PG2','PGR')],2,as.numeric))
-#2.查看PG1分布
-summary(biomark5.3$PG1)
-ggboxplot(biomark5.3,y='PG1',add='jitter',palette = 'jco')
+summary(biomark5.3)
+#3.查看PG1分布
+p1<-ggboxplot(biomark5.3,y='PG1',add='jitter',palette = 'jco')+geom_hline(yintercept = 70,color='red')
+gghistogram(biomark5.3,x='PG1',bins=100,rug=TRUE,add='median',add.params = list(color='red'))
 biomark5.3$PG1_range[biomark5.3$PG1<20]<-1
 biomark5.3$PG1_range[biomark5.3$PG1<=70 & biomark5.3$PG1>=20]<-2
 biomark5.3$PG1_range[biomark5.3$PG1<=200 & biomark5.3$PG1>70]<-3
 biomark5.3$PG1_range<-factor(biomark5.3$PG1_range,levels = c(1,2,3),labels=c('严重受损','轻度受损','相对正常'))
 table(biomark5.3$PG1_range)
-PG1_freq<-data.frame(levels=c('严重受损','轻度受损','相对正常','应激升高'),freq=c(152,3950,1818,89))
-gghistogram(biomark5.3,x='PG1',bins=40,fill='PG1_range',palette = 'jco',rug=TRUE)+
-  scale_x_continuous(breaks=c(0,20,40,70,100,150,200))
-ggplot(data=PG1_freq,aes(x=levels,y=freq,fill=levels))+geom_bar(stat='identity')+
-  geom_text(aes(label=freq))+
-  theme(legend.position = 'none')
-#3.PG2分布
+PG1_freq<-data.frame(levels=c('严重受损','轻度受损','相对正常','应激升高'),freq=c(152,3964,1825,89))
+PG1_freq$prop<-paste0(round(PG1_freq$freq/6030,2)*100,'%')
+PG1_freq
+p2<-ggpie(PG1_freq,x='freq',lab.pos='out',label='prop',fill='levels')+
+  theme(legend.title = element_blank(),legend.position = 'right')
+p3<-gghistogram(biomark5.3,x='PG1',bins=40,fill='PG1_range',palette = 'jco',rug=TRUE)+
+  scale_x_continuous(breaks=c(0,20,40,70,100,150,200))+theme(legend.title=element_blank())
+p4<-ggbarplot(PG1_freq,x='levels',y='freq',fill='levels',palette = 'jco',ylab='频数',xlab='')+
+  theme(legend.position = 'none')+geom_text(aes(label=freq),)
+ggarrange(p1,p3,p2,p4,nrow=2,ncol=2)
+#4.PG2分布
 summary(biomark5.3$PG2)
 gghistogram(biomark5.3,x='PG2',bins=30,rug=TRUE,add='median',add.params = list(color='red'))
 #gghistogram(biomark5.3,x='PG2',bins=30,rug=TRUE,add='median',color='PG1_range',add.params = list(color='red'))
-#4.PGR的
-summary(biomark5.3$PGR)
-#5.sex
-biomark5.3$sex<-factor(biomark5.3$sex,levels = c(1,2),labels = c('男','女'))
-ggboxplot(biomark5.3,x='sex',y='PG1',color='sex',palette = 'jco',add='jitter')+
-  stat_compare_means(method='wilcox.test',label='p.signif',label.x=1.5,size=8)+
-    theme(legend.position = 'none')
-#AGE
-ggscatter(biomark5.3,x='age',y='PG1')
-#PG2 and PGR
+#PG1 and PGR
+ggscatter(biomark5.3,x='PG1',y='PGR',alpha=0.2,color='PG1_range',add='reg.line')+theme(legend.title = element_blank())
+
+
+#5.PG2 and PGR
 ggscatter(biomark5.3,x='PG2',y='PGR',alpha=0.2)
 ggscatter(biomark5.3,x='PG2',y='PGR',alpha=0.2,color='PG1_range')
 ggscatter(biomark5.3,x='PG2',y='PGR',alpha=0.2,facet.by='PG1_range')
-#PG1 and PGR
-ggscatter(biomark5.3,x='PG1',y='PGR',alpha=0.2,color='PG1_range')
+mypanel <- function(x, y){
+  panel.xyplot(x, y, pch=19)
+  panel.rug(x,y)
+  panel.grid(h=-1, v=-1) # 添加水平和垂直的网格线
+  panel.lmline(x, y, col="red", lwd=1, lty=2) # 添加回归曲线
+}
+
+
+xyplot(PGR ~ PG2|PG1_range, data=biomark5.3,
+       layout=c(3,1),
+       aspect=1.5,
+       main = "PGR vs. PG2 by PG1_range",
+       xlab = "PG2",
+       ylab = "PGR",
+       panel = mypanel)
+
+#6.PG2 and PG2 and PGR
+ggplot(data=biomark5.3,aes(x=PG1,y=PGR,size=PG2))+geom_point(alpha=0.5,shape=21,colour='black',fill='cornsilk')+
+  scale_size_area()+
+  scale_colour_brewer(palette = "Set1")+
+  geom_hline(aes(yintercept = 3))+geom_vline(aes(xintercept=70))+geom_vline(aes(xintercept=20))+
+  scale_x_continuous(breaks=c(0,20,50,70,100,150,200))+scale_y_continuous(breaks=c(0,3,5,10,15,20))
+
+#7.幽门螺旋杆菌感染史与PG2、PG2、PGR的关系如何
+biomark$id<-as.numeric(biomark$id)
+baseline<-dbGetQuery(cnn,'SELECT id,name,disea17,disea18,disea20 FROM baseline WHERE year=2019 and source="示范区";')
+biomark7<-left_join(biomark,baseline,by=c('id','name'))
+biomark7.1<-biomark7%>%select(sex,age,PG1,PG2,PGR,disea17,disea18,disea20)
+biomark7.2<-biomark7.1%>%filter(PG1!='>200.0')#去除PG1>200的，89个，剩5941个
+biomark7.2[,c('PG1','PG2','PGR')]<-data.frame(apply(biomark7.2[,c('PG1','PG2','PGR')],2,as.numeric))
+summary(biomark7.2)
+ggboxplot(data=subset(biomark7.2,!is.na(disea20)),x='disea20',y='PG2',add='jitter',color='disea20')+
+  stat_compare_means(method='wilcox.test',label.x=1.5,label='p.signif',size=6,label.y=60)+
+  theme(legend.position = 'none')
+ggplot(data=biomark5.3,aes(x=PG1,y=PGR,size=PG2))+geom_point(alpha=0.5,shape=21,colour='black',fill='cornsilk')+
+  scale_size_area()+
+  scale_colour_brewer(palette = "Set1")+
+  geom_hline(aes(yintercept = 3))+geom_vline(aes(xintercept=70))+geom_vline(aes(xintercept=20))+
+  scale_x_continuous(breaks=c(0,20,50,70,100,150,200))+scale_y_continuous(breaks=c(0,3,5,10,15,20))
+
+
+
+
+
+
+
 
 
 
