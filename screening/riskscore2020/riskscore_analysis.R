3d70d149df4d41ba18a629a614b5eacbeb9f1386
shengchao9505 <shengchao9505@163.com>
2019-12-16 10:08:07 +0800

    analysis of riskscore2020

--- screening/riskscore2020/riskscore_analysis.R
+++ screening/riskscore2020/riskscore_analysis.R
@@ -2,13 +2,16 @@ library(tidyverse)
 library(openxlsx)
 library(ggpubr)
 rm(list=ls())
-screening<-read.xlsx('~/screening/PAD(街道赋值).xlsx',detectDates = TRUE)
-source('~/20年风险评分估计.R')
+screening<-read.xlsx('~/data/screening2019.xlsx',detectDates = TRUE,sheet=2)
+source('~/Rcode/screening/riskscore2020/riskscore2020.R')
 screening2<-risk_function(screening)
+screening_PAD<-screening2[which(screening2$source=='PAD'),]
+screening_epidata<-screening2[which(screening2$source=='epidata'),]
 export(screening2,'~/20年风险评分.xlsx')
 #
-score<-screening2[,199:220]
-names(score)
+score<-screening2[,193:215]
+score<-screening_epidata[,193:215]
+score<-screening_PAD[,193:215]
 rm(screening2)
 #common_risk------->check
 gghistogram(data=score,x='common_risk',y='..density..',bins=39)
@@ -28,12 +31,12 @@ plot1<-facet(ggbarplot(data=subset(cancer_score,!is.na(score)),x='score',xlab=''
           size = 6,
           vjust = 0.5,
           hjust = 0.5
-        )
-      )
+        ),plot.title=element_text(hjust=0.5)
+      )+labs(title='Epidata')
 table<-data.frame(t(apply(score[,c("lung_score",'breast_score','liver_score','gastric_score')],2,percent_value)))
 table2<-cbind(c('lung_score','breast_score',
                 'liver_score','gastric_score'),table)
 names(table2)<-c('percent','1%','2.5%','5%','10%','25%','50%','75%','90%','95%','97%','97.5','98%','99%')
 ggtable<-ggtexttable(table2,rows=NULL,theme = ttheme('mBlue'))
 ggarrange(plot1,ggtable,nrow=2,widths=c(1,20),heights=c(1,0.25))
-
+rm(list=ls())
