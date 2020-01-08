03fec22b61e2a859d0fddf40bb665a93af25bdc8
shengchao9505 <shengchao9505@163.com>
2020-01-08 20:32:37 +0800

    the analysis of CA153

--- biomarker/CA153.R
+++ biomarker/CA153.R
@@ -4,10 +4,10 @@ library(car)
 #读取数据
 source('~/Rcode/biomarker/data.R')
 biomarker2<-biomarker%>%filter(!is.na(CA153))
-biomarker3<-biomarker2%>%filter(CA153<=quantile(CA153,0.75)+IQR(CA153))
+biomarker3<-biomarker2%>%filter(CA153<=quantile(CA153,0.75)+IQR(CA153) & CA153>=quantile(CA153,0.25)-IQR(CA153))
 #---------------------------------------------------------------------------------------
 #年龄
-biomarker3%>%filter(sex==2)%>%transmute(CA153=log(CA153),age=age,age_group=age_group)%>%
+biomarker2%>%transmute(CA153=log(CA153),age=age,age_group=age_group)%>%
   ggscatterstats(                                           
     x = age,                                                   
     y = CA153, nboot = 5,                                                
@@ -31,14 +31,14 @@ biomarker3%>%filter(sex==2)%>%transmute(CA153=log(CA153),age=age,age_group=age_g
     messages = FALSE
   )
 #吸烟
-biomarker3%>%transmute(CA153=log(CA153),smoking=factor(smoking,labels=c('Never','Current','Former')))%>%
+plot.smoking1<-biomarker2%>%transmute(CA153=log(CA153),smoking=factor(smoking,labels=c('Never','Current','Former')))%>%
   ggbetweenstats(
     x = smoking,
     y =CA153 ,
     ylab='log(CA153)',
     xlab='smoking',
     nboot = 10,
-    messages = FALSE,
+    messages = FALSE,bf.message = FALSE,
     pairwise.comparisons = TRUE, 
     pairwise.display = "significant", 
     pairwise.annotation = "p.value", 
@@ -46,8 +46,35 @@ biomarker3%>%transmute(CA153=log(CA153),smoking=factor(smoking,labels=c('Never',
     ggtheme = ggthemes::theme_tufte(),
     package = "ggsci",
     palette = "default_jco",
-    title = "Relationship between CA153 and smoking",
+    title = "Relationship between CA153 and smoking(Drop the missing records)",
   )   
+plot.smoking2<-biomarker3%>%transmute(CA153=log(CA153),smoking=factor(smoking,labels=c('Never','Current','Former')))%>%
+  ggbetweenstats(
+    x = smoking,
+    y =CA153 ,
+    ylab='log(CA153)',
+    xlab='smoking',
+    nboot = 10,
+    messages = FALSE,bf.message = FALSE,
+    pairwise.comparisons = TRUE, 
+    pairwise.display = "significant", 
+    pairwise.annotation = "p.value", 
+    p.adjust.method = "fdr", 
+    ggtheme = ggthemes::theme_tufte(),
+    package = "ggsci",
+    palette = "default_jco",
+    title = "Relationship between CA153 and smoking(Drop the outlier records)",
+  )   
+combine_plots(
+  plot.smoking1, plot.smoking2,
+  nrow = 1,
+  labels = c("(a)", "(b)"),
+  title.text = "",
+  caption.text = "",
+  title.size = 14,
+  caption.size = 12
+)
+
 #BMI
 biomarker3%>%transmute(CA153=log(CA153),bmi=bmi,bmi_group=bmi_group)%>%
   ggscatterstats(                                           
@@ -70,26 +97,100 @@ biomarker3%>%transmute(CA153=log(CA153),bmi=bmi,bmi_group=bmi_group)%>%
     ysize = 1,                                                 
     type = "pearson",                                            
     title = "Relationship between CA153 and BMI",
-    messages = FALSE
+    messages = FALSE,nboot=5
   )
-#饮酒
-biomarker3%>%transmute(CA153=log(CA153),alcohol=factor(alcohol,labels=c('NO','YES')))%>%
+plot.bmi1<-biomarker3%>%transmute(CA153=log(CA153),bmi_group=factor(bmi_group,labels = c('Lower','Normal','overweight','besity')))%>%
   ggbetweenstats(
-    x = alcohol,
+    x = bmi_group,
     y =CA153,
-    ylab='log(CA1215)',
-    xlab='alcohol',
+    ylab='log(CA153)',
+    xlab='BMI',
     nboot = 10,
-    messages = FALSE,
-    pairwise.comparisons = TRUE, 
-    pairwise.display = "significant", 
-    pairwise.annotation = "p.value", #
-    p.adjust.method = "fdr", 
+    messages = FALSE,bf.message=FALSE,
+    pairwise.comparisons = TRUE, # display results from pairwise comparisons
+    pairwise.display = "significant", # display only significant pairwise comparisons
+    pairwise.annotation = "p.value", # annotate the pairwise comparisons using p-values
+    p.adjust.method = "fdr", # adjust p-values for multiple tests using this method
     ggtheme = ggthemes::theme_tufte(),
     package = "ggsci",
     palette = "default_jco",
-    title = "Relationship between CA153 and alcohol",
+    title = "Relationship between CA153 and smoking(Drop the outlier records)"
+    #title = "Relationship between CA153 and smoking(Drop the missing records)",
+    
+  )  
+plot.bmi2<-biomarker2%>%transmute(CA153=log(CA153),bmi_group=factor(bmi_group,labels = c('Lower','Normal','overweight','besity')))%>%
+  ggbetweenstats(
+    x = bmi_group,
+    y =CA153,
+    ylab='log(CA153)',
+    xlab='BMI',
+    nboot = 10,
+    messages = FALSE,bf.message=FALSE,
+    pairwise.comparisons = TRUE, # display results from pairwise comparisons
+    pairwise.display = "significant", # display only significant pairwise comparisons
+    pairwise.annotation = "p.value", # annotate the pairwise comparisons using p-values
+    p.adjust.method = "fdr", # adjust p-values for multiple tests using this method
+    ggtheme = ggthemes::theme_tufte(),
+    package = "ggsci",
+    palette = "default_jco",
+    #title = "Relationship between CA153 and smoking(Drop the outlier records)",
+    title = "Relationship between CA153 and smoking(Drop the missing records)",
+    
   )   
+combine_plots(
+  plot.bmi2,plot.bmi1,
+  nrow = 1,
+  labels = c("(a)", "(b)"),
+  title.text = " ",
+  caption.text = " ",
+  title.size = 14,
+  caption.size = 12
+)
+
+#饮酒
+plot.alcohol1<-biomarker2%>%transmute(CA153=log(CA153),alcohol=factor(alcohol,labels=c('NO','YES')))%>%
+  ggbetweenstats(
+    x = alcohol,
+    y =CA153,
+    ylab='log(CA153)',
+    xlab='alcohol',
+    nboot = 10,
+    messages = FALSE,
+    #pairwise.comparisons = TRUE, # display results from pairwise comparisons
+    #pairwise.display = "significant", # display only significant pairwise comparisons
+    #pairwise.annotation = "p.value", # annotate the pairwise comparisons using p-values
+    #p.adjust.method = "fdr", # adjust p-values for multiple tests using this method
+    ggtheme = ggthemes::theme_tufte(),
+    package = "ggsci",
+    palette = "default_jco",
+    title = "Relationship between CA153 and alcohol(Drop the missing records)",
+  )
+plot.alcohol2<-biomarker3%>%transmute(CA153=log(CA153),alcohol=factor(alcohol,labels=c('NO','YES')))%>%
+  ggbetweenstats(
+    x = alcohol,
+    y =CA153,
+    ylab='log(CA153)',
+    xlab='alcohol',
+    nboot = 10,
+    messages = FALSE,
+    #pairwise.comparisons = TRUE, # display results from pairwise comparisons
+    #pairwise.display = "significant", # display only significant pairwise comparisons
+    #pairwise.annotation = "p.value", # annotate the pairwise comparisons using p-values
+    #p.adjust.method = "fdr", # adjust p-values for multiple tests using this method
+    ggtheme = ggthemes::theme_tufte(),
+    package = "ggsci",
+    palette = "default_jco",
+    title = "Relationship between CA153 and alcohol(Drop the outlier records)",
+  ) 
+combine_plots(
+  plot.alcohol1,plot.alcohol2,
+  nrow = 1,
+  labels = c("(a)", "(b)"),
+  title.text = " ",
+  caption.text = " ",
+  title.size = 14,
+  caption.size = 12
+)
 
 #疾病史(糖尿病，高血压，高血脂，冠心病，偏头疼)(偏头疼有意义)
 biomarker3%>%transmute(CA153=log(CA153),
@@ -117,12 +218,13 @@ biomarker3%>%transmute(CA153=log(CA153),
     palette = "default_jco",
     ggstatsplot.layer = FALSE,
     messages = FALSE,
-    title.text = "The relationship between CA153 and Disease",
+    #title.text = "The relationship between CA153 and Disease(Drop the missing records)",
+    title.text = "The relationship between CA153 and Disease(Drop the outlier records)",
     nrow = 3,
   )
 
 #绝经状态
-p_menopause<-biomarker3%>%transmute(CA153=log(CA153),menopause=factor(menopause,labels=c('NO','YES')))%>%
+p_menopause1<-biomarker3%>%transmute(CA153=log(CA153),menopause=factor(menopause,labels=c('NO','YES')))%>%
   ggbetweenstats(
     x = menopause,
     y =CA153,
@@ -133,11 +235,33 @@ p_menopause<-biomarker3%>%transmute(CA153=log(CA153),menopause=factor(menopause,
     ggtheme = ggthemes::theme_tufte(),
     package = "ggsci",
     palette = "default_jco",
-    title = "Relationship between CA153 and menopause",
-  )   
+    title = "Relationship between CA153 and menopause(Drop the outlier records)",
+  ) 
+p_menopause2<-biomarker2%>%transmute(CA153=log(CA153),menopause=factor(menopause,labels=c('NO','YES')))%>%
+  ggbetweenstats(
+    x = menopause,
+    y =CA153,
+    ylab='log(CA1215)',
+    xlab='menopause',
+    nboot = 10,
+    messages = FALSE,
+    ggtheme = ggthemes::theme_tufte(),
+    package = "ggsci",
+    palette = "default_jco",
+    title = "Relationship between CA153 and menopause(Drop the missing records)",
+  ) 
+combine_plots(
+  p_menopause2,p_menopause1,
+  nrow = 1,
+  labels = c("(a)", "(b)"),
+  title.text = " ",
+  caption.text = " ",
+  title.size = 14,
+  caption.size = 12
+)
 
 #------------------------绝经年龄(有意义)
-p_agemenopau<-biomarker3%>%transmute(CA153=log(CA153),agemenopau=agemenopau)%>%
+p_agemenopau1<-biomarker3%>%transmute(CA153=log(CA153),agemenopau=agemenopau)%>%
   ggscatterstats(                                            
     x = agemenopau,                                                  
     y = CA153,                                                  
@@ -156,10 +280,43 @@ p_agemenopau<-biomarker3%>%transmute(CA153=log(CA153),agemenopau=agemenopau)%>%
     yalpha = 0.75,                                              
     xsize = 1,                                                   
     ysize = 1,                                                   
-    type = "pearson",                                            
-    title = "Relationship between CA153 and age of menopause",
+    type = "pearson",nboot=5,                                            
+    title = "Relationship between CA153 and age of menopause(Drop the outlier records)",
     messages = FALSE
   )
+p_agemenopau2<-biomarker2%>%transmute(CA153=log(CA153),agemenopau=agemenopau)%>%
+  ggscatterstats(                                            
+    x = agemenopau,                                                  
+    y = CA153,                                                  
+    xlab = "age of menopause",                 
+    ylab = "log(CA153)",                                     
+    point.alpha = 0.7,
+    point.size = 4,
+    point.color = "grey50",
+    marginal = TRUE,                                            
+    marginal.type = "density",                                   
+    centrality.para = "mean",                                    
+    margins = "both",                                           
+    xfill = "#CC79A7",                                           
+    yfill = "#009E73",                                           
+    xalpha = 0.5,                                                
+    yalpha = 0.75,                                              
+    xsize = 1,                                                   
+    ysize = 1,                                                   
+    type = "pearson",nboot=5,                                         
+    title = "Relationship between CA153 and age of menopause(Drop the missing records)",
+    messages = FALSE
+  )
+combine_plots(
+  p_agemenopau1,p_agemenopau2,
+  nrow = 1,
+  labels = c("(a)", "(b)"),
+  title.text = " ",
+  caption.text = " ",
+  title.size = 14,
+  caption.size = 12
+)
+
 #绝经状态+绝经年龄
 combine_plots(
   p_menopause, p_agemenopau,
